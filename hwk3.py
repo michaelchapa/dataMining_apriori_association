b46ee68452c82e10a09453d792a0cfe5a446a930
Michael Chapa <michaelchapa1@gmail.com>
2020-09-16 22:45:40 +0000

    Got counts, also can determine supThreshold and values exceeding it, need to implement functions

--- hwk3.py
+++ hwk3.py
@@ -1,14 +1,23 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from collections import Counter
 
 d = {'TID': ['T100', 'T200', 'T300', 'T400', 'T500', 'T600'],
-                    'Items Bought': [('E', 'K', 'M', 'N', 'O'), 
-                                     ('D', 'E', 'K', 'N', 'O', 'Y'),
-                                     ('A', 'E', 'K', 'M'),
-                                     ('C', 'K', 'M', 'U', 'Y'),
-                                     ('C', 'E', 'I', 'K', 'O'), 
-                                     ('B', 'K', 'O')]}
-data = pd.DataFrame(data = d)
+                    'Items Bought': [['E', 'K', 'M', 'N', 'O'], 
+                                     ['D', 'E', 'K', 'N', 'O', 'Y'],
+                                     ['A', 'E', 'K', 'M'],
+                                     ['C', 'K', 'M', 'U', 'Y'],
+                                     ['C', 'E', 'I', 'K', 'O'], 
+                                     ['B', 'K', 'O']]}
 
-print(data.head())
+data = pd.DataFrame(data = d)
+rowCount = (len(data.index) / 2)
+data = data.explode('Items Bought') # Turns list into individual items
+counts = data.groupby('Items Bought').count()
+counts = counts.rename(columns = {'Items Bought': 'Item', 'TID': 'Count'})
+
+filtered = counts[counts['Count'] > rowCount]
+print(filtered)
+print(rowCount)
+
